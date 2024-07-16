01b90f12dc75a18eb33344a5af326b3284acb561
SalxIeva <ieva.saldukaite@gmail.com>
2024-07-16 19:21:06 +0100

    basic pseudo code added to script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello');
+// console.log('Hello');
+
+// Create a add function
+// Create a subtract function
+// Create a multiply function
+// Create a divide function
+
+// Create a new function operate that takes an operator and 2 numbers and then calls one of the above functions on the numbers.
+
+// Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+
+// Make the calculator work! You’ll need to store the first number and second number that are input into the calculator, utilize the operator that the user selects, and then operate() on the two numbers when the user presses the “=” key.
