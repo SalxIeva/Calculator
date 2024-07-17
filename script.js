f95992c31ae6681a9ef3f0945332a992906e6b4c
SalxIeva <ieva.saldukaite@gmail.com>
2024-07-17 20:19:34 +0100

    unnesecary console logs removed

--- script.js
+++ script.js
@@ -2,23 +2,19 @@
 
 // Create a add function
 const add = (a, b) => a + b;
-
-console.log(add(3, 4));
+// console.log(add(3, 4));
 
 // Create a subtract function
 const subtract = (a, b) => a - b;
-
-console.log(subtract(4, 2));
+// console.log(subtract(4, 2));
 
 // Create a multiply function
 const multiply = (a, b) => a * b;
-
-console.log(multiply(2, 2));
+// console.log(multiply(2, 2));
 
 // Create a divide function
 const divide = (a, b) => a / b;
-
-console.log(divide(4, 2));
+// console.log(divide(4, 2));
 
 
 // Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. 
@@ -45,7 +41,7 @@ const operate = () => {
         result = 'Error';
     }
     resultInput.value = result;
-    console.log(resultInput.value);
+    console.log(`Result: ${result}`);
 
     // Resetting values og firstNum, operator and secondNum after result
     firstNum = '';
@@ -56,19 +52,14 @@ const operate = () => {
 
 // Create the functions that populate the display when you click the number buttons. You should be storing the ‘display value’ in a variable somewhere for use in the next step.
 const resultInput = document.querySelector("#result");
-// resultInput.textContent = '78594';
 const display = (val) => {
     resultInput.value += val;
-    // console.log('result input : ' + resultInput.value)
 }
 
 const buttons = document.querySelectorAll("input[type='button']");
 buttons.forEach(button => {
     button.addEventListener("click", function() {
-        // console.log(button.value);
-        // resultInput.textContent = button.value;
-        // console.log("resultInput: " + resultInput.textContent );
-        // display(button.value);
+
 
         const value = button.value;
         if (button.classList.contains("operator")) {
@@ -89,19 +80,7 @@ buttons.forEach(button => {
         } else {
             display(value);
         }
-        // console.log(`resultInput: ${resultInput.value}`);
     })
 });
 
 // Make the calculator work! You’ll need to store the first number and second number that are input into the calculator, utilize the operator that the user selects, and then operate() on the two numbers when the user presses the “=” key.
-
-// const clearBtn = document.querySelector("#clear");
-// clearBtn.addEventListener("click", function() {
-//     resultInput.value = '';
-//     firstNum = '';
-//     operator = '';
-//     secondNum = '';
-// });
-
-// const resultBtn = document.querySelector(".equals");
-// resultBtn.addEventListener("click", operate);
